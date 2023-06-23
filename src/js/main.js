08447ee8077893e98ac2a68a74fb288280e98d6a
Mateusz Tryc <mateusz.tryc420@gmail.com>
2023-06-23 21:44:56 +0200

    made services, added some pictures, made navigation working and responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,19 @@
 import { alternateBetweenHeader } from './layouts/header';
 import { handleParallaxBenefits } from './layouts/breaker';
+import { toggleSubnavigation } from './layouts/nav';
 
 window.onload = (() => {
   alternateBetweenHeader()
+  toggleSubnavigation()
 })
 
 window.onscroll = () => {
 	handleParallaxBenefits();
+  toggleSubnavigation()
 };
 
-const images = document.querySelectorAll('.gallery-img');
-const closeBtn = document.querySelectorAll('.mark');
+// const images = document.querySelectorAll('.gallery-img');
+// const closeBtn = document.querySelectorAll('.mark');
 
-closeBtn.forEach((btn) => btn.addEventListener('click', handleClosePopUp));
-images.forEach((el) => el.addEventListener('click', handlePopUp));
+// closeBtn.forEach((btn) => btn.addEventListener('click', handleClosePopUp));
+// images.forEach((el) => el.addEventListener('click', handlePopUp));
