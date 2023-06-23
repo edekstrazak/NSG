582878931940056d9f7fa3b8233057a814f7a489
PatrykKawiak <patryk.kawiak@hotmail.com>
2023-06-23 13:07:35 +0200

    added galery

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import { alternateBetweenHeader } from './layouts/header';
 import { handleParallaxBenefits } from './layouts/breaker';
-
+import { handleClosePopUp, handlePopUp } from './layouts/galery';
 // window.onload = (() => {
 //   alternateBetweenHeader()
 // })
@@ -8,3 +8,9 @@ import { handleParallaxBenefits } from './layouts/breaker';
 window.onscroll = () => {
 	handleParallaxBenefits();
 };
+
+const images = document.querySelectorAll('.gallery-img');
+const closeBtn = document.querySelectorAll('.mark');
+
+closeBtn.forEach((btn) => btn.addEventListener('click', handleClosePopUp));
+images.forEach((el) => el.addEventListener('click', handlePopUp));
