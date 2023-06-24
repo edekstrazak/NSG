583659093fda20a437c74abd14adf023854fe54f
Patryk Kawiak <patryk@MBP-Patryk.lan>
2023-06-24 13:20:13 +0200

    galery popup's

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import { alternateBetweenHeader } from './layouts/header';
 import { handleParallaxBenefits } from './layouts/breaker';
 import { toggleSubnavigation } from './layouts/nav';
-
 window.onload = (() => {
   alternateBetweenHeader()
   toggleSubnavigation()
@@ -12,8 +11,55 @@ window.onscroll = () => {
   toggleSubnavigation()
 };
 
-// const images = document.querySelectorAll('.gallery-img');
-// const closeBtn = document.querySelectorAll('.mark');
+const popUp = document.querySelector('.pop-up');
+const popUpImg = document.querySelector('.pop-up_img');
+const images = document.querySelectorAll('.gallery-img img');
+const closeBtn = document.querySelectorAll('.mark');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+let objectBg;
+let imgIndex;
+const handlePopUp = (e, index) => {
+  objectBg = e.target.getAttribute('src');
+  popUp.classList.add('show-pop-up');
+  popUpImg.style.backgroundImage = `url(${objectBg})`;
+  popUp.classList.remove('close-animation');
+  imgIndex = index + 1;
+  
+}
+images.forEach((el, index) => {
+  el.addEventListener("click", (e) => {
+    handlePopUp(e, index)
+  })
+});
 
-// closeBtn.forEach((btn) => btn.addEventListener('click', handleClosePopUp));
-// images.forEach((el) => el.addEventListener('click', handlePopUp));
+export const handleClosePopUp = () => {
+	popUp.classList.add('close-animation');
+	setTimeout(() => {
+		popUp.classList.remove('show-pop-up');
+		popUpImg.classList.remove(`${objectBg}`);
+	}, 300);
+};
+
+const handleNextImg = () => {
+if (imgIndex < images.length) {
+  imgIndex++
+  popUpImg.style.backgroundImage = `url('./src/img/img_${imgIndex}.jpeg')`;
+} else {
+  return
+}
+}
+const handlePrevImg = () => {
+if (imgIndex > 1) {
+  imgIndex--
+  popUpImg.style.backgroundImage = `url('./src/img/img_${imgIndex}.jpeg')`;
+} else {
+  return
+}
+}
+
+nextBtn.addEventListener('click', handleNextImg)
+prevBtn.addEventListener('click', handlePrevImg)
+
+
+closeBtn.forEach((btn) => btn.addEventListener('click', handleClosePopUp));
