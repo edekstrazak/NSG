5bd6bbe121dbbfdf13604face99a099f9bf75cd8
Patryk Kawiak <patryk@MBP-Patryk.lan>
2023-06-25 03:12:02 +0200

    1

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,24 @@ window.onload = (() => {
   toggleSubnavigation()
 })
 
+const handlePrallaxAboutUs = () => {
+  if(window.innerWidth < 768) return
+  const aboutUs = document.querySelector('.aboutus-content');
+  
+  let scrollValue = window.scrollY;
+  const observer = scrollValue - aboutUs.offsetTop + window.innerHeight;
+  const rate = observer / 5;
+  console.log(rate);
+
+    aboutUs.style.transform = `translateY(calc(8rem - ${rate}px))`;
+
+}
 window.onscroll = () => {
 	handleParallaxBenefits();
   toggleSubnavigation()
+  handlePrallaxAboutUs();
 };
+
+
+
 
