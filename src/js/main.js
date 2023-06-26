68af0e46b5c92202b31dd5a48834a9d1bb41c86d
Mateusz Tryc <mateusz.tryc420@gmail.com>
2023-06-26 13:21:16 +0200

    made build

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,10 @@ import { alternateBetweenHeader } from './layouts/header';
 import { handleParallaxBenefits } from './layouts/breaker';
 import { toggleSubnavigation } from './layouts/nav';
 import { handlePrallaxAboutUs } from './layouts/aboutus';
-// import AOS from 'aos';
-// import 'aos/dist/aos.css'; 
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
-// AOS.init();
+AOS.init();
 window.onload = (() => {
   alternateBetweenHeader()
   toggleSubnavigation()
