eb0c4bb027c0f8274c416ad4e84a5b00b7af3d32
PatrykKawiak <patryk.kawiak@hotmail.com>
2023-06-26 22:59:38 +0200

    added scroll and little fiexes and galery esc exit

--- src/js/layouts/galery.js
+++ src/js/layouts/galery.js
@@ -6,21 +6,23 @@ const nextBtn = document.querySelector('.next');
 const prevBtn = document.querySelector('.prev');
 let objectBg;
 let imgIndex;
+let IsPopUpActive = false;
 const handlePopUp = (e, index) => {
-  objectBg = e.target.getAttribute('src');
-  popUp.classList.add('show-pop-up');
-  popUpImg.style.backgroundImage = `url(${objectBg})`;
-  popUp.classList.remove('close-animation');
-  imgIndex = index + 1;
-  
-}
+	objectBg = e.target.getAttribute('src');
+	popUp.classList.add('show-pop-up');
+	popUpImg.style.backgroundImage = `url(${objectBg})`;
+	popUp.classList.remove('close-animation');
+	imgIndex = index + 1;
+	IsPopUpActive = true;
+};
 images.forEach((el, index) => {
-  el.addEventListener("click", (e) => {
-    handlePopUp(e, index)
-  })
+	el.addEventListener('click', (e) => {
+		handlePopUp(e, index);
+	});
 });
 
 const handleClosePopUp = () => {
+	IsPopUpActive = false;
 	popUp.classList.add('close-animation');
 	setTimeout(() => {
 		popUp.classList.remove('show-pop-up');
@@ -29,26 +31,41 @@ const handleClosePopUp = () => {
 };
 
 const handleNextImg = () => {
-if (imgIndex < images.length) {
-  imgIndex++
-  popUpImg.style.backgroundImage = `url('./src/img/img_${imgIndex}.jpeg')`;
-} else {
-  imgIndex = 1
-  popUpImg.style.backgroundImage = `url('./src/img/img_${imgIndex}.jpeg')`;
-}
-}
+	if (imgIndex < images.length) {
+		imgIndex++;
+		popUpImg.style.backgroundImage = `url('./src/img/img_${imgIndex}.jpeg')`;
+	} else {
+		imgIndex = 1;
+		popUpImg.style.backgroundImage = `url('./src/img/img_${imgIndex}.jpeg')`;
+	}
+};
 const handlePrevImg = () => {
-if (imgIndex > 1) {
-  imgIndex--
-  popUpImg.style.backgroundImage = `url('./src/img/img_${imgIndex}.jpeg')`;
-} else {
-  imgIndex = images.length
-  popUpImg.style.backgroundImage = `url('./src/img/img_${imgIndex}.jpeg')`;
-}
-}
+	if (imgIndex > 1) {
+		imgIndex--;
+		popUpImg.style.backgroundImage = `url('./src/img/img_${imgIndex}.jpeg')`;
+	} else {
+		imgIndex = images.length;
+		popUpImg.style.backgroundImage = `url('./src/img/img_${imgIndex}.jpeg')`;
+	}
+};
 
-nextBtn.addEventListener('click', handleNextImg)
-prevBtn.addEventListener('click', handlePrevImg)
+const handleClickOutside = (e) => {
+	if (IsPopUpActive) {
+		console.log(e.target);
+		if (e.target.classList.contains('pop-up')) {
+			handleClosePopUp();
+		}
+	}
+};
 
+nextBtn.addEventListener('click', handleNextImg);
+prevBtn.addEventListener('click', handlePrevImg);
 
 closeBtn.forEach((btn) => btn.addEventListener('click', handleClosePopUp));
+
+window.addEventListener('keydown', (e) => {
+	if (e.key === 'Escape' && IsPopUpActive) {
+		handleClosePopUp();
+	}
+});
+window.addEventListener('click', handleClickOutside);
