18a6195cbb5f679b7fdac88687c9405d4dc75406
PatrykKawiak <patryk.kawiak@hotmail.com>
2023-06-26 11:42:23 +0200

    idk

--- src/js/layouts/header.js
+++ src/js/layouts/header.js
@@ -2,7 +2,6 @@ const secondOptionBackground = document.querySelector('.h1-option-2-bg')
 
 export const alternateBetweenHeader = () => {
   setInterval(() => {
-    console.log('zmiana');
     secondOptionBackground.classList.toggle('max-w')
   }, 4000)
 }
--- src/js/main.js
+++ src/js/main.js
@@ -3,32 +3,40 @@ import { handleParallaxBenefits } from './layouts/breaker';
 import { toggleSubnavigation } from './layouts/nav';
 import { handlePrallaxAboutUs } from './layouts/aboutus';
 import AOS from 'aos';
-import 'aos/dist/aos.css'; 
+import 'aos/dist/aos.css';
 
 AOS.init();
-window.onload = (() => {
-  alternateBetweenHeader()
-  toggleSubnavigation()
-})
-
+window.onload = () => {
+	alternateBetweenHeader();
+	toggleSubnavigation();
+};
 
 window.onscroll = () => {
 	handleParallaxBenefits();
-  toggleSubnavigation()
-  handlePrallaxAboutUs();
+	toggleSubnavigation();
+	handlePrallaxAboutUs();
 };
 
-const input = document.querySelector('input');
+// const nav = document.querySelector('nav');
+// const navHeight = nav.offsetHeight;
 
-const handleChangeMode = () => {
-    if (input.checked) {
-        document.body.classList.add('dark')
-    }
-    
-}
-input.addEventListener('click', handleChangeMode)
+// const handleScrollSpy = () => {
+// 	let sectionArr = [];
+// 	const spySections = document.querySelectorAll('.spy');
+// 	const navItems = document.querySelectorAll(
+// 		'.nav-desktop-anchors .nav-list-item a'
+// 	);
+// 	spySections.forEach((sec) => {
+// 		if (window.scrollY <= sec.offsetTop + sec.offsetHeight - navHeight) {
+// 			sectionArr.push(sec);
+// 			const sectionName = sectionArr[0].getAttribute('id');
+// 			const currentSec = document.querySelector(
+// 				`.nav-desktop-anchors .nav-list-item [href*="${sectionName}"]`
+// 			);
+// 			navItems.forEach((item) => item.classList.remove('active-section'));
+// 			currentSec.classList.add('active-section');
+// 		}
+// 	});
+// };
 
-
-
-
-
+// window.addEventListener('scroll', handleScrollSpy);
